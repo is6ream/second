35438de7027726b4c9251b8ffa2785c6db6ff4a9
is6ream <dnlils10@icloud.com>
2024-12-12 22:41:26 +0500

    salam

--- codewars.js
+++ codewars.js
@@ -4,8 +4,9 @@ function salam(a,b){
 
 console.log(salam(6,5));
 
-let greetings = function hello(){
-    return console.log('HEllo!')
-};
+// let greetings = function hello(){
+//     return console.log('HEllo!')
+// };
 
-greetings;
+// greetings;
+
