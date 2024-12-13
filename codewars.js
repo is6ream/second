9370e1117d7ca5085c6f7d8a92ad9a38ef591f4a
is6ream <dnlils10@icloud.com>
2024-12-13 20:01:21 +0500

    Merge branch 'main' of github.com:is6ream/second

--- codewars.js
+++ codewars.js
@@ -1,12 +1,23 @@
-function salam(a,b){
+function salam(a, b) {
     return a + b;
 };
 
-console.log(salam(6,5));
+console.log(salam(6, 5));
 
 // let greetings = function hello(){
 //     return console.log('HEllo!')
 // };
 
 // greetings;
+
+class Warrior {
+    constructor(name) {
+        this.name = name;
+        this.health = 100;
+    }
+}
+
+Warrior.strike = function (enemy, swings) {
+    enemy.health = Math.max(0, enemy.health - (swings * 10));
+};
 
