e8a86cf3ffc4b3026c9beac7084365ded68bbf42
is6ream <dnlils10@icloud.com>
2024-12-13 21:33:17 +0500

    cdw

--- codewars.js
+++ codewars.js
@@ -1,23 +1,6 @@
-function salam(a, b) {
-    return a + b;
-};
-
-console.log(salam(6, 5));
-
-// let greetings = function hello(){
-//     return console.log('HEllo!')
-// };
-
-// greetings;
-
 class Warrior {
-    constructor(name) {
+    constructor(name){
         this.name = name;
         this.health = 100;
     }
-}
-
-Warrior.strike = function (enemy, swings) {
-    enemy.health = Math.max(0, enemy.health - (swings * 10));
 };
-
