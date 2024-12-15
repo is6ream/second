62cf9ca4d86dac5d24b3b0a8394af4ba9ec34116
is6ream <dnlils10@icloud.com>
2024-12-15 19:01:02 +0500

    bcknd

--- codewars.js
+++ codewars.js
@@ -1,33 +1,49 @@
 class Warrior {
-    constructor(name) {
-      this.name = name;
-      this.health = 100;
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+  }
+
+  strike(enemy, strikes) {
+    enemy.health = Math.max(0, enemy.health - (strikes * 10));
+  }
+};
+
+
+let ninja = new Warrior('Ninja');
+let samurai = new Warrior('Samurai');
+
+
+samurai.strike(ninja, 3)
+console.log(ninja.health);
+
+function longestWord(stringOfWords) {
+  let words = stringOfWords.split(' ');
+  let longestWord = "";
+
+  for (let word of words) {
+    if (word.length >= longestWord.length) {
+      longestWord = word
     }
-  
-    strike(enemy, strikes) {
-      enemy.health = Math.max(0, enemy.health - (strikes * 10));
-    }
-  };
+  }
+  return longestWord;
+};
 
 
-  let ninja = new Warrior('Ninja');
-  let samurai = new Warrior('Samurai');
+console.log(longestWord('banans mexico russionfederation'));
 
+function longestWord1(str) {
+  return str.split(' ').sort((b, a) => b.length - a.length).pop();
+}
 
-  samurai.strike(ninja,3)
-  console.log(ninja.health);
+function showTime(){
+  const now = new Date();
+  const hours = String(now.getHours()).padStart(2,"0");
+  const minutes = String(now.getMinutes()).padStart(2,"0");
+  const seconds = String(now.getSeconds()).padStart(2,"0");
 
-  function longestWord(stringOfWords){
-    let words = stringOfWords.split(' ');
-    let longestWord = "";
+  console.clear();
+  console.log(`${hours}:${minutes}:${seconds}`);
+};
 
-    for(let word of words){
-      if(word.length >= longestWord.length){
-        longestWord = word
-      }
-    }
-    return longestWord;
-  };
-
-
-  console.log(longestWord('banans mexico russionfederation'))
+setInterval(showTime,1000);
--- server.js
+++ server.js
@@ -4,22 +4,18 @@ const fs = require('fs');
 const server = http.createServer((request, response) => {
     switch (request.url) {
         case '/home': {
-            fs.readFile('pages/about.html', (err, data) => {
-                if (err) response.write(err)
-                else response.write(data)
-                response.end()
-            });
-            response.write(data);
+            setTimeout(() => {
+                const data = 'best free online course'
+                response.write(data)
+            }, 3000)
+
             break;
-        }
-        default: {
+        } default: {
             response.write('404 not found')
-            response.end()
         }
     }
-
+    response.end();
 });
 
 server.listen(3003)
-
 //19:32
