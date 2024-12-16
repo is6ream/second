f41cc8121e6cb4a73f1c11c5a278ac475dd85b5b
is6ream <dnlils10@icloud.com>
2024-12-16 21:59:15 +0500

    cdw

--- codewars.js
+++ codewars.js
@@ -36,15 +36,41 @@ function longestWord1(str) {
   return str.split(' ').sort((b, a) => b.length - a.length).pop();
 }
 
-function showTime(){
-  const now = new Date();
-  const hours = String(now.getHours()).padStart(2,"0");
-  const minutes = String(now.getMinutes()).padStart(2,"0");
-  const seconds = String(now.getSeconds()).padStart(2,"0");
+// function showTime(){
+//   const now = new Date();
+//   const hours = String(now.getHours()).padStart(2,"0");
+//   const minutes = String(now.getMinutes()).padStart(2,"0");
+//   const seconds = String(now.getSeconds()).padStart(2,"0");
 
-  console.clear();
-  console.log(`${hours}:${minutes}:${seconds}`);
+//   console.clear();
+//   console.log(`${hours}:${minutes}:${seconds}`);
+// };
+
+// setInterval(showTime,1000);
+
+
+function multi(arr) {
+  return arr.reduce((a, b) => a * b)
+}
+
+console.log(multi([1, 3, 4, 5]));
+
+function add(arr) {
+  return arr.reduce((a, b) => a + b);
+}
+
+console.log(add([1, 2, 3, 4]));
+
+function reverse(str) {
+  return str.split('').reverse().join('')
 };
 
-setInterval(showTime,1000);
+console.log(reverse('salam'));
 
+function sevenAte(str){
+  return str.split('')
+  .filter((el,i,arr) => arr[i - 1] === '7' && arr[i + 1] === '7' && el === '9')
+  .join('');
+}
+
+console.log(sevenAte('979797'))
